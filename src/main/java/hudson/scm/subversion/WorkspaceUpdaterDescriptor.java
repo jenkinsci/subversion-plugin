a6a88714f258e2f7430fdfdbe3bc20171058013b
mindless <mindless@dev.java.net>
2011-01-07 07:18:28 +0000

    [subversion] fix compilation, as in r36783 (guess kk didn't yet see all my other commits in other plugins for this :-)

--- src/main/java/hudson/scm/subversion/WorkspaceUpdaterDescriptor.java
+++ src/main/java/hudson/scm/subversion/WorkspaceUpdaterDescriptor.java
@@ -35,6 +35,6 @@ import hudson.model.Hudson;
 public abstract class WorkspaceUpdaterDescriptor extends Descriptor<WorkspaceUpdater> {
 
     public static DescriptorExtensionList<WorkspaceUpdater,WorkspaceUpdaterDescriptor> all() {
-        return (DescriptorExtensionList)Hudson.getInstance().getDescriptorList(WorkspaceUpdater.class);
+        return Hudson.getInstance().<WorkspaceUpdater,WorkspaceUpdaterDescriptor>getDescriptorList(WorkspaceUpdater.class);
     }
 }
