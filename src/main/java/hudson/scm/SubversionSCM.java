310690866961b56843946aca6ccf8fedd9ccd996
kutzi <kutzi@dev.java.net>
2011-11-15 09:45:28 +0000

    accesses to lastCompletedBuild should definitely move in the protective if block

--- src/main/java/hudson/scm/SubversionSCM.java
+++ src/main/java/hudson/scm/SubversionSCM.java
@@ -1090,10 +1090,10 @@ public class SubversionSCM extends SCM implements Serializable {
         }
 
         AbstractBuild<?,?> lastCompletedBuild = project.getLastCompletedBuild();
-        EnvVars env = lastCompletedBuild.getEnvironment(listener);
-        EnvVarsUtils.overrideAll(env, lastCompletedBuild.getBuildVariables());     
 
         if (lastCompletedBuild!=null) {
+            EnvVars env = lastCompletedBuild.getEnvironment(listener);
+            EnvVarsUtils.overrideAll(env, lastCompletedBuild.getBuildVariables());
             if (repositoryLocationsNoLongerExist(lastCompletedBuild, listener, env)) {
                 // Disable this project, see HUDSON-763
                 listener.getLogger().println(
