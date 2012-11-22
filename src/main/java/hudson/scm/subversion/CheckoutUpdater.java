jenkinsci/subversion-plugin

commit 303760992e26002aade8609beecb2fc0c1c90b92

[FIXED JENKINS-4605] Builds aborted during SCM checkout should be marked as aborted instead of failed

src/main/java/hudson/scm/subversion/CheckoutUpdater.java
@@ -32,6 +32,7 @@ import hudson.scm.SubversionSCM.External;
 import hudson.util.IOException2;
 import hudson.util.StreamCopyThread;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.tmatesoft.svn.core.SVNCancelException;
 import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.wc.SVNRevision;
@@ -59,9 +60,6 @@ public class CheckoutUpdater extends WorkspaceUpdater {
     @Override
     public UpdateTask createTask() {
         return new UpdateTask() {
-            /**
-             * 
-             */
             private static final long serialVersionUID = 8349986526712487762L;
 
             @Override
@@ -84,7 +82,9 @@ public class CheckoutUpdater extends WorkspaceUpdater {
                     File local = new File(ws, location.getLocalDir());
                     svnuc.setEventHandler(new SubversionUpdateEventHandler(new PrintStream(pos), externals, local, location.getLocalDir()));
                     svnuc.doCheckout(location.getSVNURL(), local.getCanonicalFile(), SVNRevision.HEAD, getRevision(location), SVNDepth.INFINITY, true);
-
+                } catch (SVNCancelException e) {
+                    listener.error("Subversion checkout has been canceled");
+                    throw (InterruptedException)new InterruptedException().initCause(e);
                 } catch (SVNException e) {
                     e.printStackTrace(listener.error("Failed to check out " + location.remote));
                     return null;

src/main/java/hudson/scm/subversion/UpdateUpdater.java
@@ -33,6 +33,7 @@ import hudson.scm.SubversionSCM.ModuleLocation;
 import hudson.scm.SubversionSCM.SvnInfo;
 import hudson.triggers.SCMTrigger;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.tmatesoft.svn.core.SVNCancelException;
 import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.SVNErrorCode;
 import org.tmatesoft.svn.core.SVNException;
@@ -132,7 +133,9 @@ public class UpdateUpdater extends WorkspaceUpdater {
                 preUpdate(location, local);
                 listener.getLogger().println("Updating " + location.remote);
                 svnuc.doUpdate(local.getCanonicalFile(), r, SVNDepth.INFINITY, true, false);
-
+            } catch (SVNCancelException e) {
+                listener.error("Subversion update has been canceled");
+                throw (InterruptedException)new InterruptedException().initCause(e);
             } catch (final SVNException e) {
                 if (e.getErrorMessage().getErrorCode() == SVNErrorCode.WC_LOCKED) {
                     // work space locked. try fresh check out

commit 7155ad99177e88f1083afdacbf0d2fbc3f39d425

Fixed: missing authentication data should result in build FAILURE (instead of ABORTED). This was broken in commit 303760992e26002aade8609beecb2fc0c1c90b92

src/main/java/hudson/scm/subversion/CheckoutUpdater.java
@@ -94,8 +94,13 @@ public class CheckoutUpdater extends WorkspaceUpdater {
                     
                     svnuc.doCheckout(location.getSVNURL(), local.getCanonicalFile(), SVNRevision.HEAD, r, SVNDepth.INFINITY, true);
                 } catch (SVNCancelException e) {
-                    listener.error("Subversion checkout has been canceled");
-                    throw (InterruptedException)new InterruptedException().initCause(e);
+                    if (isAuthenticationFailedError(e)) {
+                        e.printStackTrace(listener.error("Failed to check out " + location.remote));
+                        return null;
+                    } else {
+                        listener.error("Subversion checkout has been canceled");
+                        throw (InterruptedException)new InterruptedException().initCause(e);
+                    }
                 } catch (SVNException e) {
                     e.printStackTrace(listener.error("Failed to check out " + location.remote));
                     return null;

src/main/java/hudson/scm/subversion/UpdateUpdater.java
@@ -143,8 +143,13 @@ public class UpdateUpdater extends WorkspaceUpdater {
                 listener.getLogger().println("Updating " + location.remote + " to revision " + revisionName);
                 svnuc.doUpdate(local.getCanonicalFile(), r, SVNDepth.INFINITY, true, false);
             } catch (SVNCancelException e) {
-                listener.error("Subversion update has been canceled");
-                throw (InterruptedException)new InterruptedException().initCause(e);
+                if (isAuthenticationFailedError(e)) {
+                    e.printStackTrace(listener.error("Failed to check out " + location.remote));
+                    return null;
+                } else {
+                    listener.error("Subversion update has been canceled");
+                    throw (InterruptedException)new InterruptedException().initCause(e);
+                }
             } catch (final SVNException e) {
                 Throwable cause = e;
                 do {

src/main/java/hudson/scm/subversion/WorkspaceUpdater.java
@@ -33,6 +33,7 @@ import hudson.scm.SubversionSCM.External;
 import hudson.scm.SubversionSCM.ModuleLocation;
 import hudson.scm.SvnClientManager;
 import org.kohsuke.stapler.export.ExportedBean;
+import org.tmatesoft.svn.core.SVNCancelException;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationProvider;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
 import org.tmatesoft.svn.core.wc.SVNRevision;
@@ -65,6 +66,13 @@ public abstract class WorkspaceUpdater extends AbstractDescribableImpl<Workspace
     @Override
     public WorkspaceUpdaterDescriptor getDescriptor() {
         return (WorkspaceUpdaterDescriptor)super.getDescriptor();
+    }
+    
+    protected static boolean isAuthenticationFailedError(SVNCancelException e) {
+        // this is very ugly. SVNKit (1.7.4 at least) reports missing authentication data as a cancel exception
+        // "No credential to try. Authentication failed"
+        // See DefaultSVNAuthenticationManager#getFirstAuthentication
+        return e.getMessage().contains("No credential to try");
     }
 
     /**