f74ea686fcb68aab0b3ff9730f4754cb0af02d27
Oleg Nenashev <nenashev@synopsys.com>
2013-10-30 19:59:08 +0400

    [FIXED JENKINS-20344] - SVN_REVISION is not exported if server url end by / with further @HEAD suffix SvnHelper function also truncates ending slashes => hash search works correctly. Also added some tests...
    Resolves https://issues.jenkins-ci.org/browse/JENKINS-20344
    
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

--- src/main/java/hudson/scm/subversion/SvnHelper.java
+++ src/main/java/hudson/scm/subversion/SvnHelper.java
@@ -8,20 +8,28 @@ import org.tmatesoft.svn.core.wc.SVNRevision;
  * @author kutzi
  */
 public class SvnHelper {
+    private static final String REGEX_END_SLASHES = "\\/*$";
 
     /**
-     * Cuts off any optional '@revisionnr' from the end of the url string.
+     * Cuts off any optional '@revisionnr' and slashes from the end of the url string.
      */
     public static String getUrlWithoutRevision(String remoteUrlPossiblyWithRevision) {
         int idx = remoteUrlPossiblyWithRevision.lastIndexOf('@');
         int slashIdx = remoteUrlPossiblyWithRevision.lastIndexOf('/');
+        
+        // Substitute optional '@revisionnr'
+        String substititedString = remoteUrlPossiblyWithRevision;
         if (idx > 0 && idx > slashIdx) {
             String n = remoteUrlPossiblyWithRevision.substring(idx + 1);
             SVNRevision r = SVNRevision.parse(n);
             if ((r != null) && (r.isValid())) {
-                return remoteUrlPossiblyWithRevision.substring(0, idx);
+                substititedString = remoteUrlPossiblyWithRevision.substring(0, idx);
             }
         }
-        return remoteUrlPossiblyWithRevision;
+        
+        // Substitute slashes at the end
+        substititedString = substititedString.replaceAll(REGEX_END_SLASHES, "");
+        
+        return substititedString;
     }
 }
