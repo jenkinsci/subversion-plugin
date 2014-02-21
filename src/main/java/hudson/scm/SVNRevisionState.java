89298d5b48055e7f49122761a949b16be3803aaa
Simon Wiest <simon.wiest@web.de>
2014-02-21 09:40:23 +0100

    Raised visibility of SVNRevisionState to public [JENKINS-21907].

--- src/main/java/hudson/scm/SVNRevisionState.java
+++ src/main/java/hudson/scm/SVNRevisionState.java
@@ -7,14 +7,14 @@ import java.util.Map;
  * {@link SCMRevisionState} for {@link SubversionSCM}. {@link Serializable} since we compute
  * this remote.
  */
-final class SVNRevisionState extends SCMRevisionState implements Serializable {
+public final class SVNRevisionState extends SCMRevisionState implements Serializable {
     /**
      * All the remote locations that we checked out. This includes those that are specified
      * explicitly via {@link SubversionSCM#getLocations()} as well as those that
      * are implicitly pulled in via svn:externals, but it excludes those locations that
      * are added via svn:externals in a way that fixes revisions.
      */
-    final Map<String,Long> revisions;
+    public final Map<String,Long> revisions;
 
     SVNRevisionState(Map<String, Long> revisions) {
         this.revisions = revisions;
