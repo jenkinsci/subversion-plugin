992c4e54780a10df286fb19214caad0c2ea30049
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2018-09-20 11:33:33 +0200

    clear system property after the test

--- src/test/java/jenkins/scm/impl/subversion/SubversionSCMFileSystemBuilderTest.java
+++ src/test/java/jenkins/scm/impl/subversion/SubversionSCMFileSystemBuilderTest.java
@@ -1,16 +1,23 @@
 package jenkins.scm.impl.subversion;
 
 import hudson.scm.SubversionSCM;
+import org.junit.After;
 import org.junit.Test;
 
+import static jenkins.scm.impl.subversion.SubversionSCMFileSystem.DISABLE_PROPERTY;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SubversionSCMFileSystemBuilderTest {
 
+    @After
+    public void after(){
+        System.clearProperty(DISABLE_PROPERTY);
+    }
+
     @Test
     public void builderDisable(){
-        System.setProperty(SubversionSCMFileSystem.DISABLE_PROPERTY, "true");
+        System.setProperty(DISABLE_PROPERTY, "true");
         SubversionSCMFileSystem.BuilderImpl builder = new SubversionSCMFileSystem.BuilderImpl();
         assertFalse(builder.supports(new SubversionSCM("svn://svn.example.com/trunk")));
         assertFalse(builder.supports(new SubversionSCMSource("id", "/")));
@@ -18,12 +25,12 @@ public class SubversionSCMFileSystemBuilderTest {
 
     @Test
     public void builderEnable(){
-        System.setProperty(SubversionSCMFileSystem.DISABLE_PROPERTY, "");
+        System.clearProperty(DISABLE_PROPERTY);
         SubversionSCMFileSystem.BuilderImpl builder = new SubversionSCMFileSystem.BuilderImpl();
         assertTrue(builder.supports(new SubversionSCM("svn://svn.example.com/trunk")));
         assertTrue(builder.supports(new SubversionSCMSource("id", "/")));
 
-        System.setProperty(SubversionSCMFileSystem.DISABLE_PROPERTY, "false");
+        System.setProperty(DISABLE_PROPERTY, "false");
         builder = new SubversionSCMFileSystem.BuilderImpl();
         assertTrue(builder.supports(new SubversionSCM("svn://svn.example.com/trunk")));
         assertTrue(builder.supports(new SubversionSCMSource("id", "/")));
